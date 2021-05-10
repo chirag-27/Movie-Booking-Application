bac440c5278cdd60f5f90df361e6ab76ca2b35ff
chirag-27 <solankichirag2703@gmail.com>
2021-05-10 23:37:00 +0530

    Checkpoint 3

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -8,7 +8,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -19,7 +18,6 @@ const useStyles = makeStyles((theme) => ({
   },
   gridList: {
     flexWrap: 'nowrap',
-    
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
     transform: 'translateZ(0)',
   },
@@ -40,7 +38,7 @@ export function SingleLineGridList() {
       <GridList className={classes.gridList} cols={6} cellHeight={250}>
         {moviesData.map((tile) => (
           <GridListTile key={tile.poster_url}>
-            <img src={tile.poster_url} alt={tile.title} />
+            <img src={tile.poster_url} alt={tile.title}/>
             <GridListTileBar
               title={tile.title}
               
@@ -53,19 +51,51 @@ export function SingleLineGridList() {
 }
 
 
+export function TitlebarGridList() {
+  const classes = useStyles();
+  const handleClick = function() {
+    console.log("Redirecting to details page"); //to be redirected to details page when available 
+  }
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={350} className={classes.gridListReleasedMovies} cols={4}>
+        {moviesData.map((tile) => (
+          <GridListTile key={tile.poster_url}>
+            <img src={tile.poster_url} alt={tile.title} className={classes.movieImg} style={{cursor:'pointer'}} onClick={() => handleClick()}/>
+            <GridListTileBar
+              title={tile.title}
+              subtitle={<span>Released Dates: {tile.release_date.toLocaleString()}</span>}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
+
 
 class Home extends Component {
     render() {
       return (
-        
+
         <div className="main-container">
             <Header/>
             <div className="movies-heading">
-                <span>Upcoming Movies</span>
+              <span>Upcoming Movies</span>
             </div>
             <div>
-                <SingleLineGridList />
-            </div>    
+              <SingleLineGridList />
+            </div>
+            <div className="flex-container">
+              
+              <div className="left">
+                  <TitlebarGridList />
+              </div>
+              
+              <div className="right">
+                  {/*to be added*/}
+              </div>
+            </div>   
         </div>
       );
     }
